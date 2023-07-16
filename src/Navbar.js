5b81bd617444577054976aefd1b16403af0f7156
lior.buzaglo@e.braude.ac.il <lior.buzaglo@e.braude.ac.il>
2023-07-16 20:14:52 +0300

    new import

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,13 @@
+import React from 'react';
+
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <img className='nav-img' src="https://imgtr.ee/images/2023/06/01/S83jq.th.png"></img>
-            <h3> The UP-GRADE website</h3>
-            <div className="links">
-                <a href="/">  Home  </a>
-                <a href="/create">  New Course  </a>
-                <a>  </a>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav>
+      <a href="/" className="logo-link">
+      </a>
+      {/* Other navbar content */}
+    </nav>
+  );
+};
+
 export default Navbar;
